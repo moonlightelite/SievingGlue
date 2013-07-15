3bd2dc588d73091c784cc50c28604aed75a95414
timlau <timlau@timlau-VirtualBox.(none)>
2013-07-15 09:17:24 -0700

    Trying to fix some SQL injection issues

---
+++ db.php
@@ -0,0 +1,31 @@
+<?php
+class DB
+{
+  private static $getInitial;
+
+  public static function getInitial() {
+    if (self::$getInitial == null)
+      self::$getInitial = new DB();
+    return self::$getInitial;
+  }
+
+  public function __construct()
+  {
+  }
+
+  private static $db_path = 'db/work.db';
+
+  public static function workjobExist($name){
+    exec("sqlite3 " + DB_PATH + " \"select name from job where condition='WORKING' and name='".$name."' limit 1;\"", $output);
+    return count($output) != 0;
+  }
+
+  public static function getAllJobs(){
+    $path = self::$db_path;
+    $q = "sqlite3 $path \"select * from job\"";
+    exec($q, $output);
+    print $q;
+    return $output;
+  }
+}
+?>
